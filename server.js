e40d9843da6abe7ea867f96a1c5d57a6e8a33c87
M.Abdullah <abdullahluqmanbro9@gmail.com>
2024-07-29 12:49:04 +0500

    error resolved

--- server.js
+++ server.js
@@ -1,19 +1,32 @@
-const express = require('express')
+const express = require('express');
 const app = express();
 require('dotenv').config();
-const PORT = process.env.PORT;
-var cors = require('cors')
-const db = require('./db.js');
-const bodyParser = require('body-parser')
-app.use(bodyParser.json()); 
-app.use(cors())
+const PORT = process.env.PORT || 3001; // Default port to 3001 if not specified
+const cors = require('cors');
+const db = require('./db'); // Ensure db.js is correctly set up
+const bodyParser = require('body-parser');
 
-app.get('/',(req,res)=>{res.send('Hello World')})
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
 const contactRoute = require('./router/contactRoute');
-app.use('/',contactRoute)
+app.use('/', contactRoute);
 
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
 
+// 404 handler for undefined routes
+app.use((req, res) => {
+  res.status(404).send('Not Found');
+});
 
-app.listen(PORT,()=>{
-    console.log('Server is Runnig on port',PORT)
-})
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});

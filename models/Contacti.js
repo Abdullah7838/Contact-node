256a316d091f5033636ac74ab7d962f1801ccbc5
M.Abdullah <abdullahluqmanbro9@gmail.com>
2024-09-15 11:56:20 +0500

    Final

--- models/Contacti.js
+++ models/Contacti.js
@@ -17,6 +17,10 @@ const contactSchema = mongoose.Schema({
     msg:{
         type:String,
         required:true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
     }
 });
 
